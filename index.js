c5ea407ca626a63bffa72e6652f0b462556d99ca
mdsiyad <engsiyad2020@gmail.com>
2023-01-12 10:04:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion () {
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "How are you today?",
+          });
+          console.log(completion.data.choices[0].text);
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+runCompletion();
